4b0f32c892f935cc385a06e7a1492027e8479cb3
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-15 11:10:58 +1000

    no need to check for plone.app.relationfield presence in testing as it is a dependency for testing.

--- plone/app/layout/testing.py
+++ plone/app/layout/testing.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.layout.viewlets.content import HAS_PLONE_APP_RELATIONFIELD
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
@@ -16,18 +15,15 @@ class Fixture(PloneSandboxLayer):
         # Load ZCML
         import plone.app.layout
         self.loadZCML(package=plone.app.layout)
-        # check for relationfield support
-        if HAS_PLONE_APP_RELATIONFIELD:
-            # we need behaviors setup
-            import plone.app.dexterity
-            self.loadZCML(package=plone.app.dexterity)
-            # and the behavior we want to test
-            import plone.app.relationfield
-            self.loadZCML(package=plone.app.relationfield)
+        # we need behaviors setup
+        import plone.app.dexterity
+        self.loadZCML(package=plone.app.dexterity)
+        # and the behavior we want to test
+        import plone.app.relationfield
+        self.loadZCML(package=plone.app.relationfield)
 
     def setUpPloneSite(self, portal):
-        if HAS_PLONE_APP_RELATIONFIELD:
-            applyProfile(portal, 'plone.app.relationfield:default')
+        applyProfile(portal, 'plone.app.relationfield:default')
 
 
 FIXTURE = Fixture()
