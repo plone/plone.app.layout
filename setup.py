f24190a5b6f0b19dc0b242c7c5ea45063e0e160d
Maurits van Rees <maurits@vanrees.org>
2016-02-15 12:49:52 +0100

    Preparing release 2.3.14
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.3.14.dev0'
+version = '2.3.14'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
