b990cacedb5402ff1013f1ede771562f3762546f
Maurits van Rees <maurits@vanrees.org>
2019-08-29 22:51:15 +0200

    Back to development: 2.8.5
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.8.4'
+version = '2.8.5.dev0'
 
 long_description = \
     open("README.rst").read() + "\n" + open("CHANGES.rst").read()
