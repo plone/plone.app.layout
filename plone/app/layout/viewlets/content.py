c30c35d4d0b26c499f445e909b281c219deb1afa
Johannes Raggam <thetetet@gmail.com>
2021-09-23 18:49:34 +0200

    Use pat-display-time from Patternslib in the toolbar and remove pat-moment from the history view.

--- plone/app/layout/viewlets/content.py
+++ plone/app/layout/viewlets/content.py
@@ -497,15 +497,14 @@ class ContentHistoryViewlet(WorkflowHistoryViewlet):
 
     def toLocalizedTime(self, time, long_format=None, time_only=None):
         """Convert time to localized time"""
-        return DateTime(time).ISO()
-        # util = getToolByName(self.context, 'translation_service')
-        # return util.ulocalized_time(
-        #     time,
-        #     long_format,
-        #     time_only,
-        #     self.context,
-        #     domain='plonelocales'
-        # )
+        util = getToolByName(self.context, 'translation_service')
+        return util.ulocalized_time(
+            time,
+            long_format,
+            time_only,
+            self.context,
+            domain='plonelocales'
+        )
 
 
 class ContentHistoryView(ContentHistoryViewlet):
