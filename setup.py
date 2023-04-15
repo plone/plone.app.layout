003e4773236026abdedfc7348f00537ca1a23f05
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-15 11:04:50 +0200

    Back to development: 4.0.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "4.0.3"
+version = "4.0.4.dev0"
 
 long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
