62091e844c55d7061e9e6b0766b17b5c1331ebaa
Jens W. Klein <jk@kleinundpartner.at>
2023-04-27 13:44:23 +0200

    typo

--- plone/app/layout/navigation/root.py
+++ plone/app/layout/navigation/root.py
@@ -6,6 +6,6 @@ deprecated(
     getNavigationRoot="plone.base.navigationroot:get_navigation_root",
 )
 deprecated(
-    "Import as get_navigation_root from plone.base.navigationroot instead (will be removed in Plone 7)",
+    "Import as get_navigation_root_object from plone.base.navigationroot instead (will be removed in Plone 7)",
     getNavigationRootObject="plone.base.navigationroot:get_navigation_root_object",
 )
