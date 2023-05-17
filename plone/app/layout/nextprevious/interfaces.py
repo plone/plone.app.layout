587eb40c3c79d89dc19bf216bf3426d107b02f86
Roman <roman.kysil@redturtle.it>
2023-05-17 12:06:39 +0200

    Fix the INextPreviousProvide path

--- plone/app/layout/nextprevious/interfaces.py
+++ plone/app/layout/nextprevious/interfaces.py
@@ -4,5 +4,5 @@ import zope.deferredimport
 zope.deferredimport.initialize()
 zope.deferredimport.deprecated(
     "Import from plone.app.dexterity instead (will be removed in Plone 7)",
-    INextPreviousProvider="plone.base.interfaces.nextprevious:INextPreviousProvider",
+    INextPreviousProvider="plone.app.dexterity.behaviors.nextprevious:INextPreviousProvider",
 )
