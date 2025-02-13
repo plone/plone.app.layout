60dda84a69f8b17349ddcc6f8313269fe60f6bcf
Peter Mathis <peter.mathis@kombinat.at>
2025-02-13 15:43:10 +0100

    fix dependencies

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         "plone.memoize",
         "plone.portlets",
         "plone.protect",
-        "plone.uuid",
         "Products.CMFEditions >=1.2.2",
         "Products.GenericSetup",
         "Products.statusmessages",
